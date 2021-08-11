fdb7a7728f612ec17eb484535a1bb184aedf8970
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-10 22:48:00 -0400

    Agregar archivos de proyecto.

---
+++ Numero aleatorio 5,9,1,1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Numero_aleatorio_5_9_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aleatorio();
+        }
+        public static void aleatorio()
+        {
+            Random r = new Random();
+            int aleatorio = r.Next(1,100);
+            int oportunidad = 0;
+            int contesta = 0;
+            int intento = 0;
+            Console.WriteLine("Introduce un numero entre 1 y 100, tratando de acertar el numero aleatoreo ");
+            do
+            {
+                intento++;
+                oportunidad = 6 - intento;
+                
+                contesta = Convert.ToInt32(Console.ReadLine());
+                if (contesta == aleatorio)
+                {
+                    Console.WriteLine("El numero que introduciste es: " + contesta);
+                    Console.WriteLine(" y el numero a la azar es: " + aleatorio);
+                    Console.WriteLine("Has acertado con " + intento + " Intentos");
+                }
+                else
+                {
+                    Console.WriteLine("El numero que introduciste es: " + contesta);
+                    //Console.WriteLine(" y el numero a la azar es: " + aleatorio);
+                    Console.WriteLine("No has acertado te quedan " + oportunidad + " Intentos");
+                    Console.WriteLine("");
+                    Console.WriteLine(" ");
+                    
+                }
+                if (intento==6)
+                {
+                    Console.WriteLine("Has Fracasado "+oportunidad+" Intentos ");
+                    Console.WriteLine("El numero al Azar es: "+aleatorio);
+                }
+                else
+                {
+                    Console.WriteLine("Intentalo de nuevo ");
+                }
+
+
+
+
+            } while ((contesta != aleatorio) && (intento != 6));
+
+        }
+    }
+}
